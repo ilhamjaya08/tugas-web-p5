38327f4b235ff8f27e59129f191ea0cb7b6574eb
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-05-07 08:27:59 +0700

    fix: layout

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -18,16 +18,12 @@ export const About = () => {
             <div className="pb-6">
               <h2 className="text-3xl md:text-4xl font-bold">
                 <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-                  About{" "}
+                  Tentang{" "}
                 </span>
-                Company
+                Kami
               </h2>
               <p className="text-xl text-muted-foreground mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit.
+                Kami adalah perusahaan pengembang game dan perangkat lunak edukatif dan kreatif dari Indonesia
               </p>
             </div>
 
